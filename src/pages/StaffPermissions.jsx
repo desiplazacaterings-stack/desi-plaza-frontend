af6eba19722ce4f905bfcd7002c47e965b99d38f
Dhanraj <rajmv06@gmail.com>
2025-12-29 14:43:55 +0530

    fix: Correct import path in StaffPermissions.jsx
    - Change import from '../../config' to '../config'
    - Fixes build error in Vercel deployment

--- src/pages/StaffPermissions.jsx
+++ src/pages/StaffPermissions.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import API_ENDPOINTS from "../../config";
+import API_ENDPOINTS from "../config";
 import "./StaffPermissions.css";
 
 function StaffPermissions() {
